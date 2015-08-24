76ec2c65f05b1b3d57c8ecce6d2488560498337e
ETCasella <Triplay@Keiths-MacBook-Pro.local>
2015-08-24 19:17:38 -0400

    Word Game Final and Working
    All bugs resolved, should work as needed

---
+++ Assets/__Scripts/Letter.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Letter : MonoBehaviour {
+
+	private char _c;
+	public TextMesh tMesh;
+	public Renderer tRend;
+
+	public bool big = false;
+
+	void Awake(){
+		tMesh = GetComponentInChildren<TextMesh> ();
+		tRend = tMesh.GetComponent<Renderer> ();
+		visable = false;
+	}
+
+	public char c {
+		get {
+			return(_c);
+		}
+		set {
+			_c = value;
+			tMesh.text = _c.ToString();
+		}
+	}
+
+	public string str {
+		get {
+			return(_c.ToString ());
+		}
+		set {
+			c = value [0];
+		}
+	}
+
+	public bool visable {
+		get {
+			return(tRend.enabled);
+		}
+		set {
+			tRend.enabled = value;
+		}
+	}
+
+	public Color color {
+		get {
+			return(this.GetComponent<Renderer> ().material.color);
+		}
+		set {
+			this.GetComponent<Renderer> ().material.color = value;
+		}
+	}
+
+	public Vector3 pos {
+		set {
+			this.transform.position = value;
+		}
+	}
+}
+			       
+
